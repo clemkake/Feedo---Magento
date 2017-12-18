2b05022a83db286ba0f34df03d12997989abd3f0
kone dotemin clement <elitedoh@gmail.com>
2017-12-18 21:52:38 +0530

    Update IndexController.php

--- Rgx/Feedo/controllers/IndexController.php
+++ Rgx/Feedo/controllers/IndexController.php
@@ -1,57 +1,51 @@
-<?php 
-class Rgx_Feedo_IndexController extends Mage_Core_Controller_Front_Action{
-	/**
-   * Index action
-   * @access public
-   * @return void
-   */
-	public function indexAction() {
-		
-		//echo "testit";
-	 	//$this->loadLayout();
-     	//$this->renderLayout();
+<?php   
+    echo '<?xml version="1.0"?>';
+    echo '<rss xmlns:g="http://base.google.com/ns/1.0" version="2.0">';
+	echo '<channel>';
 
-		include_once  "includes/headers.php";
-		include_once  "includes/feedstructure.php";
-	}
-	
-	public function FeedAction() {
+	 /**
+	 *Need to make a loop here
+	 */
+	  $_products = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('sku')->addAttributeToSelect('name')->addAttributeToSelect('image')->addAttributeToSelect('msrp_display_actual_price_type') ->addAttributeToSelect('news_from_date') ->addAttributeToSelect('id') ->addAttributeToSelect('_category');
 
-		echo "google";
-         
-		   $_products = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('sku')->addAttributeToSelect('name')->addAttributeToSelect('image')->addAttributeToSelect('price')->addAttributeToSelect('news_from_date')->addAttributeToSelect('id')->addAttributeToSelect('apparel_type')->addAttributeToSelect('url_path')->addAttributeToSelect('description')->addAttributeToSelect('is_in_stock');
-
-		   print_r($_products);
-
-		/*	foreach ($_products as $product){
-				$availibilty = 'in stock';
-
-				if($product->getData('is_in_stock')){
-					$availibilty = 'in stock';
-				} else {
-					$availibilty = 'out of stock';
-				}
-					$description = $product->getData('description');
-					$sku = $product->getData('sku');
-					$name = $product->getData('name');
-					$image =$_SERVER['HTTP_HOST'].$product->getData('image');
-					$price = $product->getData('price');
-					$category = $product->getData('_root_category');
-					$url = $_SERVER['HTTP_HOST'].'/'.$product->getData('url_path');
-				
-					echo $msrp_display_actual_price_type.'<br>';
-					echo $news_from_date.'<br>';
-					echo $image.'<br>';
-					echo $sku .'<br>';
-					echo $name .'<br>';  
-					echo $id.'<br>';
-					echo $price.'<br>';
-					echo $category .'<br>';
-					echo $description .'<br>';    }  */
 		 
+	foreach ($_products as $product){
 
-		}
+		$id = $product->getData('id');
+		$sku = $product->getData('sku');
+		$name = $product->getData('name');
+		$image = $product->getData('image');
+		$msrp_display_actual_price_type = $product->getData('msrp_display_actual_price_type');
+		$news_from_date = $product->getData('news_from_date');
+		$category = $product->getData('_category');
+         
+	echo '<item>';
+	echo '<g:id>'. $sku.'</g:id>';
+	echo '<g:title><![CDATA['.$name.']]></g:title>';
+	echo '<description><![CDATA[]]></description>';
+	echo '<link></link>';
+	echo '<g:image_link>'.$image.'</g:image_link>';
+	echo '<g:condition></g:condition>';
+	echo '<g:availability></g:availability>';
+	echo '<g:brand></g:brand>';
+	echo '<g:price>'.$msrp_display_actual_price_type.'</g:price>';
+ 	echo '<g:gtin></g:gtin>'; 
+	echo '<g:mpn>'.$sku.'</g:mpn>';
+	echo '<g:identifier_exists></g:identifier_exists>';
+	echo '<g:product_type>'.$category.'</g:product_type>';
+	echo '<g:google_product_category></g:google_product_category>';
+	echo '<g:shipping>';
+   	echo '<g:country></g:country>';
+   		echo '<g:service></g:service>';
+   		echo '<g:price></g:price>';
+	echo '</g:shipping>';
+
+	echo '</item>';
 	
-}
+	}
+/** end of the loop */
+
+echo '</channel>';
+echo '</rss>';
 
 ?>
